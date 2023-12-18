8fdc6d2a43da287a85ae0370ca9bdec988a10712
JasonEl1 <1eliseijas@hdsb.ca>
2023-12-18 15:59:57 -0500

    add initial test files

---
+++ matplotlibsimpletest.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+pyplot.plot(xpoints, ypoints)
+pyplot.show()
---
+++ plotssimpletest.jl
@@ -0,0 +1,7 @@
+using Plots
+
+#y = randn(10)
+y=
+plot(y)
+
+savefig("testfig.png")
