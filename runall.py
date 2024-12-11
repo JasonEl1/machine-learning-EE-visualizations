389e8501b092d51c5d5f2d081d6d0474936ecc58
JasonEl1 <1eliseijas@hdsb.ca>
2024-12-11 14:06:13 -0500

    improve compatibility of runall.py

--- runall.py
+++ runall.py
@@ -1,7 +1,20 @@
 import os
 import subprocess
 
+system_type = os.name
+if system_type == "posix":
+    python_name = "python3"
+    pip_name = "pip3"
+elif system_type == "nt":
+    python_name = "python"
+    pip_name = "pip"
+else:
+    exit()
+
 outer_folder = os.getcwd()
+
+subprocess.run([f"{pip_name}","install","-r",os.path.join(outer_folder,"requirements.txt")])
+
 inner_folders = [f.path for f in os.scandir(outer_folder) if f.is_dir()]
 
 for folder in inner_folders:
@@ -10,6 +23,6 @@ for folder in inner_folders:
     for filename in files:
         if filename.endswith(".py"):
             path = os.path.join(folder,filename)
-            subprocess.run(['python',path])
+            subprocess.run([f"{python_name}",path])
 
 print(f"Done running all python scripts in {outer_folder}")
